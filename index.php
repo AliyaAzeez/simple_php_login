f4b066cad015f0084154690567f0fcdd879b8b0a
Aliya <aliya@jusfocus.com>
2021-07-06 14:41:50 +0100

    initial login php commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+include("connection.php");
+include("functions.php");
+
+$user_data = check_login($con);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Test website</title>
+    </head>
+    <body>
+        <a href="logout.php">Logout</a>
+        <h1>Welcome to the test website</h1>
+        <p>Hello! <?php echo $user_data['user_name'];?></p>
+    </body>
+</html>
